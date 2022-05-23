20f9cc16957dfd45b7ead3ec3bc3373af2c3dd4f
Zeeshan Haider <zhadier39@gmail.com>
2022-05-24 04:03:53 +0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # authenticated :user do
-  #   root 'calendars#index', as: :authenticated_root
-  # end
+
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
+  
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
+
   unauthenticated :user do
     root 'pages#splash', as: :unauthenticated_root
+  end
+
+  resources :groups, only: %i[index new create destroy] do
+    resources :expenses, only: %i[index new create destroy]
   end
 end
