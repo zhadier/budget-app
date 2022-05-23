a4d63f56d6cd940d904145450cf5f51e41ff6533
Zeeshan Haider <zhadier39@gmail.com>
2022-05-24 04:05:53 +0500

    Update model for user with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :groups
+  has_many :expenses, foreign_key: 'author_id'
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
