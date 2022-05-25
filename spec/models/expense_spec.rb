691b2cf66bc1bd2742d42a7edb86b74e7aa1ba82
Zeeshan Haider <zhadier39@gmail.com>
2022-05-25 05:59:36 +0500

    Add specs for groups, expenses and users

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {create(:user)}
+  subject { create(:expense, author_id: user.id) }
+  before { subject.save }
+
+  it 'Expense does not have errors if name and amount is present' do
+    expect(subject).to be_valid
+  end
+
+  it 'Name should not be nil' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Name should be atleast 3 chars long' do
+    subject.name = 'ab'
+    expect(subject).to_not be_valid
+  end
+
+  it 'Amount is not nil' do
+    subject.amount = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Amount is > 0' do
+    subject.amount = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'Has a user' do
+    expect(subject.author).to eql(user)
+  end
+
+  context 'total_expenses' do
+    let(:group) {create(:group, total_expenses:5)}
+    it 'Increases total expense for group when created' do
+      group.expenses.create(name:"zee", amount: 1, author_id: user.id, groups:[group])
+      expect(group.total_expenses).to eq(1 + 5)
+    end
+
+    it 'Decreases total expense for group when destroyed' do
+      
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    subject { create(:user) }
+    before { subject.save }
+
+    it 'User does not have errors if name, email and password given' do
+      expect(subject).to be_valid
+    end
+
+    it 'Name should not be nil' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Email should not be nil' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Has many expenses' do
+      create_list :expense, 5, author_id:subject.id
+      expect(subject.expenses.length).to eql(5)
+    end
+
+    it 'Has many groups' do
+      create_list :group, 3, user_id:subject.id
+      expect(subject.groups.length).to eql(3)
+    end
 end
