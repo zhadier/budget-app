1822de4119755dccddfeced6a9d7e01a5a3d574e
Zeeshan Haider <zhadier39@gmail.com>
2022-05-25 05:56:54 +0500

    Update model for expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,8 +1,8 @@
 class Expense < ApplicationRecord
-  belongs_to :user, class_name: 'User'
+  belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :groups
 
-  validates :name, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
   validates :amount, numericality: { greater_than: 0 }
 
   after_create :add_total_expense
