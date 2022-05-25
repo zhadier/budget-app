d5a612aba74b8b184cd1e4bd2268559230fd99a9
Zeeshan Haider <zhadier39@gmail.com>
2022-05-25 05:58:55 +0500

    Update views for expenses

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :expenses
   has_one_attached :icon
 
-  validates :name, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
   validates :total_expenses, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :icon, presence: true
 end
